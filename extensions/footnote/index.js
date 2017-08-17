9cb89c116022a32a23615f1d129015d06e5e76e4
nessunkim <nessunkim@gmail.com>
2017-08-17 19:52:40 +0900

    footnotes

---
+++ extensions/footnote/index.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const striptags = require('striptags');
+const ArrayMap = require('../../lib/utils/ArrayMap');
+
+function registerFootnote($item, parsingData) {
+  const footnoteGroups = parsingData.structureData.footnoteGroups;
+  const lastFootnoteGroup = footnoteGroups[footnoteGroups.length - 1];
+  const footnoteName = $item.attr('name');
+  const content = $item.html().trim();
+
+  if (footnoteName === undefined) {
+    parsingData.structureData.numOfFootnotes += 1;
+    const footnote = {
+      isNamed: false,
+      absoluteNumber: parsingData.structureData.numOfFootnotes,
+      relativeNumber: lastFootnoteGroup.size + 1,
+      content,
+      stripedContent: striptags(content).replace(/"/g, '&quot;'),
+    };
+    lastFootnoteGroup.push(footnote);
+    return `<sup id="footnote-${footnote.absoluteNumber}-back" title="${footnote.stripedContent}"><a href="#footnote-${footnote.absoluteNumber}">[${footnote.relativeNumber}]</a></sup>`;
+  }
+
+  let footnote = lastFootnoteGroup.get(footnoteName);
+  if (footnote) {
+    footnote.innerNumber += 1;
+  } else {
+    parsingData.structureData.numOfFootnotes += 1;
+    footnote = {
+      innerNumber: 0,
+      isNamed: true,
+      absoluteNumber: parsingData.structureData.numOfFootnotes,
+      relativeNumber: lastFootnoteGroup.size + 1,
+      content,
+      stripedContent: striptags(content).replace(/"/g, '&quot;'),
+    };
+    lastFootnoteGroup.set(footnoteName, footnote);
+  }
+  return `<sup id="footnote-${footnoteName}_${footnote.absoluteNumber}-${footnote.innerNumber}-back" title="${footnote.stripedContent}"><a href="#footnote-${footnoteName}_${footnote.absoluteNumber}">[${footnote.relativeNumber}]</a></sup>`;
+}
+
+function showFootnotes($item, parsingData) {
+  const footnoteGroups = parsingData.structureData.footnoteGroups;
+  const lastFootnoteGroup = footnoteGroups[footnoteGroups.length - 1];
+  footnoteGroups.push(new ArrayMap());
+  const rows = [];
+  for (const [key, footnote] of lastFootnoteGroup) {
+    if (footnote.isNamed) {
+      let row = `<li id="footnote-${key}_${footnote.absoluteNumber}"><span>↑`;
+      for (let i = 0; i <= footnote.innerNumber; i += 1) {
+        row += ` <sup><a href="#footnote-${key}_${footnote.absoluteNumber}-${i}-back">${footnote.relativeNumber}.${i}</a></sup>`;
+      }
+      row += `</span> <span>${footnote.content}</span>`;
+      rows.push(row);
+    } else {
+      rows.push(`<li id="footnote-${footnote.absoluteNumber}"><span><a href="#footnote-${footnote.absoluteNumber}-back">↑</a></span> <span>${footnote.content}</span>`);
+    }
+  }
+  return `<ol class="references">\n${rows.join('\n')}\n</ol>`;
+}
+
+function hook($item, parsingData) {
+  if (!parsingData.structureData.footnoteGroups) {
+    parsingData.structureData.footnoteGroups = [new ArrayMap()];
+    parsingData.structureData.numOfFootnotes = 0;
+  }
+  const tagName = $item[0].name;
+  if (tagName === 'references') {
+    return showFootnotes($item, parsingData);
+  }
+  return registerFootnote($item, parsingData);
+}
+
+module.exports.import = (helper) => {
+  helper.parserHook.setXmlHook({
+    type: 'afterParsing',
+    selector: 'ref,references',
+    hook,
+  });
+};

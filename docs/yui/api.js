01c1e2af1cc915067d2d824ff1b3faa6c82e6a43
NessunKim <nessunkim@gmail.com>
2017-11-17 21:36:30 +0900

    set documentation

--- docs/yui/api.js
+++ docs/yui/api.js
@@ -1,43 +1,12 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
-    "classes": [
-        "Article",
-        "Namespace",
-        "Redirection",
-        "RenameLog",
-        "Revision",
-        "User",
-        "UserGroup",
-        "UserSignature",
-        "Wikitext"
-    ],
+    "classes": [],
     "modules": [
-        "Article",
-        "Namespace",
-        "Redirection",
-        "RenameLog",
-        "Revision",
-        "User",
-        "UserGroup",
-        "UserSignature",
-        "Wikitext",
-        "models",
         "modules_WikiRenderer_Nodes_Node.js",
         "modules_WikiRenderer_Nodes_TextNode.js",
-        "modules_WikiRenderer_Preprocessor_Nodes_NodeList.js",
-        "recentChanges"
+        "modules_WikiRenderer_Preprocessor_Nodes_NodeList.js"
     ],
     "allModules": [
-        {
-            "displayName": "Article",
-            "name": "Article",
-            "description": "Provides Article model."
-        },
-        {
-            "displayName": "models",
-            "name": "models",
-            "description": "Provides models of Sequelize ORM."
-        },
         {
             "displayName": "modules/WikiRenderer/Nodes/Node.js",
             "name": "modules_WikiRenderer_Nodes_Node.js",
@@ -52,51 +21,6 @@ YUI.add("yuidoc-meta", function(Y) {
             "displayName": "modules/WikiRenderer/Preprocessor/Nodes/NodeList.js",
             "name": "modules_WikiRenderer_Preprocessor_Nodes_NodeList.js",
             "description": "List of node clases used for prerendering."
-        },
-        {
-            "displayName": "Namespace",
-            "name": "Namespace",
-            "description": "Provides Namespace model."
-        },
-        {
-            "displayName": "recentChanges",
-            "name": "recentChanges",
-            "description": "Cache and provides recent changes."
-        },
-        {
-            "displayName": "Redirection",
-            "name": "Redirection",
-            "description": "Provides Redirection model."
-        },
-        {
-            "displayName": "RenameLog",
-            "name": "RenameLog",
-            "description": "Provides RenameLog model."
-        },
-        {
-            "displayName": "Revision",
-            "name": "Revision",
-            "description": "Provides Revision model."
-        },
-        {
-            "displayName": "User",
-            "name": "User",
-            "description": "Provides User model."
-        },
-        {
-            "displayName": "UserGroup",
-            "name": "UserGroup",
-            "description": "Provides UserGroup model."
-        },
-        {
-            "displayName": "UserSignature",
-            "name": "UserSignature",
-            "description": "Provides UserSignature model."
-        },
-        {
-            "displayName": "Wikitext",
-            "name": "Wikitext",
-            "description": "Provides Wikitext model."
         }
     ],
     "elements": []

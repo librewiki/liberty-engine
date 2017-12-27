3830f028f40da0ba56e1f97490a27f78dca08599
NessunKim <nessunkim@gmail.com>
2017-12-27 13:03:35 +0900

    fix code style

--- tests/parser/parser.js
+++ tests/parser/parser.js
@@ -36,67 +36,52 @@ describe('Parser', () => {
   describe('Text', () => {
     it('should be rendered correctly', async () => {
       const result = await wikitextParser.parseRender({ wikitext: 'asdf' });
-      result.html.should.be.eql(
-        `<p>
+      result.html.should.be.eql(`<p>
     asdf
-</p>`
-      );
+</p>`);
     });
     it('should be rendered correctly (unicode)', async () => {
       const result = await wikitextParser.parseRender({ wikitext: 'Iñtërnâtiônàlizætiøn☃💩' });
-      result.html.should.be.eql(
-        `<p>
+      result.html.should.be.eql(`<p>
     Iñtërnâtiônàlizætiøn☃💩
-</p>`
-      );
+</p>`);
     });
   });
 
   describe('Link', () => {
     it('should be rendered correctly', async () => {
       const result = await wikitextParser.parseRender({ wikitext: '[[asdf]]' });
-      result.html.should.be.eql(
-        `<p>
+      result.html.should.be.eql(`<p>
     <a class="new" href="asdf">asdf</a>
-</p>`
-      );
+</p>`);
     });
     it('should be rendered correctly (unicode)', async () => {
       const result = await wikitextParser.parseRender({ wikitext: '[[Iñtërnâtiônàlizætiøn☃💩]]' });
-      result.html.should.be.eql(
-        `<p>
+      result.html.should.be.eql(`<p>
     <a class="new" href="Iñtërnâtiônàlizætiøn☃💩">Iñtërnâtiônàlizætiøn☃💩</a>
-</p>`
-      );
+</p>`);
     });
   });
 
   describe('Heading', () => {
     it('should be rendered correctly', async () => {
       const result = await wikitextParser.parseRender({ wikitext: '==aaa==' });
-      result.html.should.be.eql(
-        '<h2><span id="s-1" class="liberty-wiki-heading"><a href="#toc-title">1</a> aaa</span></h2>'
-      );
+      result.html.should.be.eql('<h2><span id="s-1" class="liberty-wiki-heading"><a href="#toc-title">1</a> aaa</span></h2>');
     });
     it('should be rendered correctly', async () => {
       const result = await wikitextParser.parseRender({ wikitext: '===aaa===' });
-      result.html.should.be.eql(
-        '<h3><span id="s-1" class="liberty-wiki-heading"><a href="#toc-title">1</a> aaa</span></h3>'
-      );
+      result.html.should.be.eql('<h3><span id="s-1" class="liberty-wiki-heading"><a href="#toc-title">1</a> aaa</span></h3>');
     });
     it('should be rendered correctly (unicode)', async () => {
       const result = await wikitextParser.parseRender({ wikitext: '===Iñtërnâtiônàlizætiøn☃💩===' });
-      result.html.should.be.eql(
-        '<h3><span id="s-1" class="liberty-wiki-heading"><a href="#toc-title">1</a> Iñtërnâtiônàlizætiøn☃💩</span></h3>'
-      );
+      result.html.should.be.eql('<h3><span id="s-1" class="liberty-wiki-heading"><a href="#toc-title">1</a> Iñtërnâtiônàlizætiøn☃💩</span></h3>');
     });
   });
 
   describe('Table of Contents', () => {
     it('should be rendered correctly', async () => {
       const result = await wikitextParser.parseRender({ wikitext: '__TOC__\n==aaa==' });
-      result.html.should.be.eql(
-        `<div id="toc" class="toc">
+      result.html.should.be.eql(`<div id="toc" class="toc">
     <div id="toc-title">
         <h2>${i18next.t('LibertyParser:TableOfContents')}</h2>
     </div>
@@ -108,13 +93,11 @@ describe('Parser', () => {
 </div>
 
 
-<h2><span id="s-1" class="liberty-wiki-heading"><a href="#toc-title">1</a> aaa</span></h2>`
-      );
+<h2><span id="s-1" class="liberty-wiki-heading"><a href="#toc-title">1</a> aaa</span></h2>`);
     });
     it('should be rendered correctly (unicode)', async () => {
       const result = await wikitextParser.parseRender({ wikitext: '__TOC__\n==Iñtërnâtiônàlizætiøn☃💩==' });
-      result.html.should.be.eql(
-        `<div id="toc" class="toc">
+      result.html.should.be.eql(`<div id="toc" class="toc">
     <div id="toc-title" class="toc-title">
         <h2>${i18next.t('LibertyParser:TableOfContents')}</h2>
     </div>
@@ -126,28 +109,23 @@ describe('Parser', () => {
 </div>
 
 
-<h2><span id="s-1" class="liberty-wiki-heading"><a href="#toc-title">1</a> Iñtërnâtiônàlizætiøn☃💩</span></h2>`
-      );
+<h2><span id="s-1" class="liberty-wiki-heading"><a href="#toc-title">1</a> Iñtërnâtiônàlizætiøn☃💩</span></h2>`);
     });
   });
 
   describe('Magic Word', () => {
     it('should be rendered correctly', async () => {
       const result = await wikitextParser.parseRender({ wikitext: '{{SITENAME}}' });
-      result.html.should.be.eql(
-        `<p>
+      result.html.should.be.eql(`<p>
     ${Setting.get('wikiName')}
-</p>`
-      );
+</p>`);
     });
 
     it('should be rendered correctly', async () => {
       const result = await wikitextParser.parseRender({ wikitext: '{{SERVER}}' });
-      result.html.should.be.eql(
-        `<p>
+      result.html.should.be.eql(`<p>
     ${Setting.get('domain')}
-</p>`
-      );
+</p>`);
     });
   });
 });

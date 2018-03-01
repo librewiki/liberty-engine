2bf7c5ea1fb076142e9e82fe531b2dc5aa9b2606
NessunKim <nessunkim@gmail.com>
2018-03-01 22:13:23 +0900

    use model.Setting to pass notifier-telegram api token

--- extensions/notifier-telegram/index.js
+++ extensions/notifier-telegram/index.js
@@ -6,31 +6,20 @@ const _ = require('lodash');
 const TelegramBot = require('node-telegram-bot-api');
 const models = require('../../lib/models');
 
-const env = process.env.NODE_ENV || 'development';
-const config = require('../../config/config.json')[env];
-
-if (config['notifier-telegram'] && config['notifier-telegram'].token) {
-  const bot = new TelegramBot(config['notifier-telegram'].token, { polling: true });
-
+module.exports.import = (helper) => {
+  const token = models.Setting.get('notifier-telegram:token');
+  const bot = new TelegramBot(token, { polling: true });
   bot.onText(/\/start/, async (msg) => {
     const chatIds = models.Setting.get('notifier-telegram:chatIds') || [];
     chatIds.push(msg.chat.id);
     await models.Setting.set('notifier-telegram:chatIds', _.uniq(chatIds));
     bot.sendMessage(msg.chat.id, '등록되었습니다.');
   });
-
-
   const notifier = {
     async send(data) {
       const chatIds = models.Setting.get('notifier-telegram:chatIds') || [];
       await Promise.all(chatIds.map(id => bot.sendMessage(id, data.message)));
     },
   };
-
-  module.exports.import = (helper) => {
-    helper.addNotifier(notifier);
-  };
-} else {
-  module.exports.import = () => {};
-}
-
+  helper.addNotifier(notifier);
+};
